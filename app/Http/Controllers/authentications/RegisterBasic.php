edd3cef616e06e8d48ae688f4c57d04263d239e8
Felipe Clayton <felipeclayton007@gmail.com>
2024-03-22 10:37:36 -0300

    feat: register validation

--- app/Http/Controllers/authentications/RegisterBasic.php
+++ app/Http/Controllers/authentications/RegisterBasic.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use GuzzleHttp\Promise\Create;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class RegisterBasic extends Controller
 {
@@ -18,15 +19,31 @@ class RegisterBasic extends Controller
 
  public function registra(Request $request)
   {
-    
-  User::Create([
-    'identificador' => $request->identificador,
-    'email' => $request->email,
-    'password' => $request->password,
+    $data = [
+      'identificador' => $request->identificador,
+      'email' => $request->email,
+      'password' => $request->password,
+    ];
 
-  ]);
- 
-  return redirect()->route('auth-login-basic');
+    $validate = Validator::make($data, 
+    [
+      'identificador' => 'required',
+      'email' => 'required|email|unique:users,email',
+      'password' => 'required|min:6'
+    ],
+    [
+      'identificador' => 'Identificador inválido!',
+      'email' => 'Email inválido!',
+      'password' => 'Senha inválida!'
+    ]);
+
+    if($validate->fails()) {
+      return redirect()->back()->withErrors(['credenciais' => 'Credenciais informadas são inválidas.']);
+    }
+
+    User::Create($data);
+  
+    return redirect()->route('auth-login-basic');
 
   }
 
